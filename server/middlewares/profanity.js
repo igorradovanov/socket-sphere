fc52c79353561fff123c9fff4a8fee361b122a75
Igor Radovanov <igor@radovanovigor.com>
2023-12-26 12:26:02 +0100

    Tasks

--- server/middlewares/profanity.js
+++ server/middlewares/profanity.js
@@ -3,4 +3,14 @@
 //Example:  bad words: ['bad', 'good', 'words']
 //Result:   bad words: ['***', 'good', '*****']
 
-//TODO v2: Add profanity filter to chat
+//TODO Tasks: Add profanity filter to chat
+
+//Pseudocode:
+// 1. Define an array of profanity words.
+// 2. Create a function to filter the profanity words from the chat text.
+// 3. Iterate through each word in the chat text.
+// 4. Check if the word is present in the profanity words array.
+// 5. If the word is a profanity word, replace it with asterisks of the same length.
+// 6. Return the filtered chat text.
+
+// TODO v2 Goal: Add profanity filter to chat
