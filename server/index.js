f1b6c9bd984fba5af2bac21b09ec0c75856cd897
Igor Radovanov <igor@radovanovigor.com>
2023-12-26 12:17:24 +0100

    Refactor chatSocket

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import { Server } from 'socket.io';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import helmet from 'helmet';
+import { chatSocket } from './sockets/chatSocket.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -24,42 +25,7 @@ const httpServer = app.listen(PORT, () => {
 
 const io = new Server(httpServer);
 
-//TODO - Create a message broker class to handle all the socket events
-
-io.on('connection', (socket) => {
-  // Initial
-  console.log(`User connected: ${socket.id}`);
-
-  // On connection to user
-  socket.emit('message', 'Welcome to the Sphere Chat! 💬');
-
-  // On connnection to all users
-
-  socket.broadcast.emit('message', `${socket.id.substring(0, 5)} has joined the chat`);
-
-  // On disconnect
-  socket.on('disconnect', () => {
-    console.log(`User disconnected: ${socket.id}`);
-  });
-
-  // On disconnect to all users
-
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('message', `${socket.id.substring(0, 5)} has left the chat`);
-  });
-
-  // On message received
-  socket.on('message', (data) => {
-    console.log(data);
-    io.emit('message', `${socket.id.substring(0, 5)}: ${data}`);
-  });
-
-  // On activity
-  socket.on('activity', (name) => {
-    socket.broadcast.emit('activity', name);
-  });
-
-});
+chatSocket(io);
 
 export { app };
  
---
+++ server/sockets/chatSocket.js
@@ -0,0 +1,37 @@
+const chatSocket = io => {
+    io.on('connection', socket => {
+        // Initial
+        console.log(`User connected: ${socket.id}`);
+
+        // On connection to user
+        socket.emit('message', 'Welcome to the Sphere Chat! 💬');
+
+        // On connnection to all users
+
+        socket.broadcast.emit('message', `${socket.id.substring(0, 5)} has joined the chat`);
+
+        // On disconnect
+        socket.on('disconnect', () => {
+            console.log(`User disconnected: ${socket.id}`);
+        });
+
+        // On disconnect to all users
+
+        socket.on('disconnect', () => {
+            socket.broadcast.emit('message', `${socket.id.substring(0, 5)} has left the chat`);
+        });
+
+        // On message received
+        socket.on('message', (data) => {
+            console.log(data);
+            io.emit('message', `${socket.id.substring(0, 5)}: ${data}`);
+        });
+
+        // On activity
+        socket.on('activity', (name) => {
+            socket.broadcast.emit('activity', name);
+        });
+    });
+}
+
+export { chatSocket };
